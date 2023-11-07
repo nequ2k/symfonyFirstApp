79b5af7cf95248723095c1ac6b83ce6f4cdc64bf
Wiktor Balcerek <automat1516@o2.pl>
2023-11-07 19:29:09 +0100

    assets introduction

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,22 +2,39 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Movie;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies/', name: 'app_movies')]
+
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
-        $movies = ['Avengers: Endgame', 'Inception', 'Loki', 'Black Widow'];
+        // findAll = SELECT * FROM movies;
+        // find() = SELECT * FROM movies where id = 5;
+        // findBy() = SELECT * FROM movies ORDER BY id DESC;
+        // findOneBy() = SELECT * FROM movies WHERE id = 6 AND title = 'The Dark Knight' ORDER BY id DESC;
+        //SELECT COUNT(*) WHERE id = 5
 
-        return $this->render('index.html.twig', array(
-            'movies' => $movies,
-        ));
+        $repository = $this->em->getRepository(Movie::class);
+
+        $movies = $repository->getClassName();
+        dd($movies);
+        return $this->render('index.html.twig');
     }
+
 
 
 }
