0a48111e99905d4ba6795e0bf37dee587d10bcae
Wiktor Balcerek <automat1516@o2.pl>
2023-12-03 13:52:58 +0100

    learning how to show database content in page!

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -13,27 +13,22 @@ use App\Entity\Movie;
 class MoviesController extends AbstractController
 {
 
-    private $em;
-
     private $movieRepository;
+
     public function __construct(MovieRepository $movieRepository)
     {
         $this->movieRepository = $movieRepository;
     }
 
+
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
-        // findAll = SELECT * FROM movies;
-        // find() = SELECT * FROM movies where id = 5;
-        // findBy() = SELECT * FROM movies ORDER BY id DESC;
-        // findOneBy() = SELECT * FROM movies WHERE id = 6 AND title = 'The Dark Knight' ORDER BY id DESC;
-        //SELECT COUNT(*) WHERE id = 5
-
         $movies = $this->movieRepository->findAll();
 
+
         return $this->render('movies/index.html.twig', [
-            'movies' => $movies
+            'movies' => $movies,
         ]);
     }
 
